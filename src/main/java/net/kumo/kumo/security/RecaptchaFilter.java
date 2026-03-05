0f7fa5fe65d2f35816360aaf4992f2d8b6c45824
YoonByungjin <dbsqudwls77@gmail.com>
2026-03-05 17:28:09 +0900

    3/4 리캡챠, admin은 모든 페이지 접속허용

--- src/main/java/net/kumo/kumo/security/RecaptchaFilter.java
+++ src/main/java/net/kumo/kumo/security/RecaptchaFilter.java
@@ -16,7 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Optional;
-
+//안녕하세요
 @Slf4j
 @Component
 @RequiredArgsConstructor
--- src/main/java/net/kumo/kumo/util/RecaptchaService.java
+++ src/main/java/net/kumo/kumo/util/RecaptchaService.java
@@ -1,5 +1,5 @@
 package net.kumo.kumo.util;
-
+// 안녕하세요
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
