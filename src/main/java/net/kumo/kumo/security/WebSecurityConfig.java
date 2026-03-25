c7890c0161d6e650b5597ed0542f5649458917a2
YoonByungjin <dbsqudwls77@gmail.com>
2026-03-25 17:57:45 +0900

    Update: Refine project structure and security configuration

--- src/main/java/net/kumo/kumo/security/WebSecurityConfig.java
+++ src/main/java/net/kumo/kumo/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -90,6 +91,6 @@ public class WebSecurityConfig implements WebMvcConfigurer {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
     }
 }
